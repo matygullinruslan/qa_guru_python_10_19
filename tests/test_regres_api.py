f80ffea916581e92241eb7d5af682ea1c2ee2812
matygullinruslan <matygullinruslan@gmail.com>
2024-04-19 00:19:07 +0300

    API test

--- tests/test_regres_api.py
+++ tests/test_regres_api.py
@@ -3,7 +3,6 @@ from jsonschema import validate
 from schemas.schemas import post_users, list_users, update_users, error_register, successful_registr
 
 
-
 def test_create_user_status_code():
     url = 'https://reqres.in'
     endpoint = '/api/users'
@@ -28,7 +27,7 @@ def test_get_list_user_status_code():
     url = 'https://reqres.in'
     endpoint = '/api/users'
     params = {"page": "2"}
-    response = requests.get(url+endpoint, json=params)
+    response = requests.get(url + endpoint, json=params)
     assert response.status_code == 200
     body = response.json()
     validate(body, list_users)
@@ -42,14 +41,12 @@ def test_delete_user_status_code():
     assert response.text == ""
 
 
-
 def test_get_user_not_found_status_code():
     url = 'https://reqres.in'
     endpoint = '/api/unknown/23'
-    response = requests.get(url+endpoint)
+    response = requests.get(url + endpoint)
     assert response.status_code == 404
     assert response.json() == {}
-
 
 
 def test_unsuccessful_registration_user_status_code():
@@ -62,7 +59,8 @@ def test_unsuccessful_registration_user_status_code():
     validate(body, error_register)
     assert response.json() == {"error": "Missing password"}
 
-#схема для request
+
+
 
 def test_successful_registration_user_status_code():
     url = 'https://reqres.in'
@@ -74,7 +72,8 @@ def test_successful_registration_user_status_code():
     validate(body, successful_registr)
     assert response.json() == {"id": 4, "token": "QpwL5tke4Pnpja7X4"}
 
-#регистрация с неверным email
+
+# регистрация с неверным email
 def test_invalid_email_registration_user_status_code():
     url = 'https://reqres.in'
     endpoint = '/api/register'
@@ -82,13 +81,3 @@ def test_invalid_email_registration_user_status_code():
     response = requests.post(url + endpoint, json=params)
     assert response.status_code == 400
     assert response.json() == {"error": "Note: Only defined users succeed registration"}
-
-
-
-
-
-
-# def test_successful_registration_user_status_code():
-
-
-
